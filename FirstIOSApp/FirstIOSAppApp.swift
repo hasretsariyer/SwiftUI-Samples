0e4ec227af1db1bef5dd3b2655701aafe918c514
Hasret Sariyer <hasretsariyer@gmail.com>
2020-11-07 19:10:38 +0300

    Add group

--- FirstIOSApp/FirstIOSAppApp.swift
+++ FirstIOSApp/FirstIOSAppApp.swift
@@ -18,6 +18,6 @@ struct FirstIOSAppApp: App {
 
 struct FirstIOSAppApp_Previews: PreviewProvider {
     static var previews: some View {
-        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+        ContentView()
     }
 }
