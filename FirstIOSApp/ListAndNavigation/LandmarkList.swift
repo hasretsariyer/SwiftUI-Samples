f2b985e6c4337dae5084a2286ccbc498e9730962
Hasret Sariyer <hasretsariyer@gmail.com>
2020-11-08 22:40:53 +0300

    Add dark mode

--- FirstIOSApp/ListAndNavigation/LandmarkList.swift
+++ FirstIOSApp/ListAndNavigation/LandmarkList.swift
@@ -23,5 +23,6 @@ struct LandmarkList: View {
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
         LandmarkList()
+            .environment(\.colorScheme, .dark)
     }
 }
